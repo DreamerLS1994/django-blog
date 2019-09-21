82fd161eaf7ec1520fffe8513f25e130b90e6c26
Jerry <454239755@qq.com>
2019-09-21 11:36:56 +0800

    二次更新

--- README.txt
+++ README.txt
@@ -1,10 +1,2 @@
-django=2.2.2
-django-allauth=0.39.1
-django-crispy-forms=1.7.2 
-django-imagekit=4.0.2
-pillow=6.0.0
-xadmin=0.6.1
-django-ckeditor=5.7.1
-
-
+管理员账号密码：
 adminblog/blog1234
--- mysite/apps/mainapp/templates/mainapp/one/mv.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cp /root/one.txt  /home/mysite/iHuiCorner/apps/blog/templates/blog/one/one.txt 
-cp /root/one.txt  /home/mysite/jerrycoding2.0/mysite/apps/mainapp/templates/mainapp/one/one.txt 
